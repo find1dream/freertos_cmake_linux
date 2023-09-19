d8fd2f7846bcc5d20875a1fe60714225ac16181f
find1dream <clinfeng.jlu@gmail.com>
2023-09-19 04:12:56 +0000

    fix

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 /* The rate at which data is sent to the queue.  The times are converted from
 milliseconds to ticks using the pdMS_TO_TICKS() macro. */
 #define mainTASK_SEND_FREQUENCY_MS pdMS_TO_TICKS(500UL)
-#define mainTIMER_SEND_FREQUENCY_MS pdMS_TO_TICKS(1000UL)
+#define mainTIMER_SEND_FREQUENCY_MS pdMS_TO_TICKS(10UL)
 
 /* The number of items the queue can hold at once. */
 #define mainQUEUE_LENGTH (2)
